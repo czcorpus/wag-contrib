69d143d1cd57e7efbaf868487eb7d114c43eae05
Martin Zimandl <martin.zimandl@gmail.com>
2022-11-23 12:28:40 +0100

    added ujcKLA tile

---
+++ src/js/tiles/custom/ujcKLA/api.ts
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 Martin Zimandl <martin.zimandl@gmail.com>
+ * Copyright 2022 Institute of the Czech National Corpus,
+ *                Faculty of Arts, Charles University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { HTTP } from 'cnc-tskit';
+import { Observable, of as rxOf } from 'rxjs';
+import { IApiServices } from '../../../appServices';
+import { cachedAjax$ } from '../../../page/ajax';
+import { ResourceApi, SourceDetails, HTTPHeaders, IAsyncKeyValueStore } from '../../../types';
+import { DataStructure } from './common';
+
+
+export interface UjcKLAArgs {
+    q:string;
+}
+
+
+export class UjcKLAApi implements ResourceApi<UjcKLAArgs, DataStructure> {
+
+    private readonly cache:IAsyncKeyValueStore;
+
+    private readonly apiURL:string;
+
+    private readonly customHeaders:HTTPHeaders;
+
+    private readonly apiServices:IApiServices;
+
+
+    constructor(cache:IAsyncKeyValueStore, apiURL:string, apiServices:IApiServices) {
+        this.apiURL = apiURL;
+        this.customHeaders = apiServices.getApiHeaders(apiURL) || {};
+        this.cache = cache;
+        this.apiServices = apiServices;
+    }
+
+    call(args:UjcKLAArgs):Observable<DataStructure> {
+        return cachedAjax$<DataStructure>(this.cache)(
+            HTTP.Method.GET,
+            this.apiURL,
+            args,
+        );
+    }
+
+    getSourceDescription(tileId:number, lang:string, corpname:string):Observable<SourceDetails> {
+        return rxOf({
+            tileId,
+            title: this.apiServices.importExternalMessage({
+                'cs-CZ': 'Kartotéka lexikálního archivu (1911–1991)',
+                'en-US': 'Kartotéka lexikálního archivu (1911–1991) UNTRANSLATED'
+            }),
+            description: this.apiServices.importExternalMessage({
+                'cs-CZ': 'Kartotéka novočeského lexikálního archivu obsahuje excerpční materiál, na jehož základě vznikly všechny reprezentativní výkladové slovníky češtiny. Základní, tzv. 1. materiál archivu, sloužil jako východisko pro zpracování Příručního slovníku jazyka českého. Sběr jazykových dat probíhal na základě promyšlených excerpčních pravidel (z roku 1911, 1917). Lístkové výpisky byly pořizovány zejména z umělecké literatury, ale i z literatury odborné, výběrově byly excerpovány noviny, časopisy a překladová literatura. Jako dolní hranice pro excerpci byl stanoven rok 1770, díky čemuž se podařilo zdokumentovat též češtinu z obrozeneckého období. V průběhu let byly sbírky lexikálního materiálu postupně doplňovány v souladu s aktuálními úkoly pracoviště.',
+                'en-US': 'Kartotéka novočeského lexikálního archivu obsahuje excerpční materiál, na jehož základě vznikly všechny reprezentativní výkladové slovníky češtiny. Základní, tzv. 1. materiál archivu, sloužil jako východisko pro zpracování Příručního slovníku jazyka českého. Sběr jazykových dat probíhal na základě promyšlených excerpčních pravidel (z roku 1911, 1917). Lístkové výpisky byly pořizovány zejména z umělecké literatury, ale i z literatury odborné, výběrově byly excerpovány noviny, časopisy a překladová literatura. Jako dolní hranice pro excerpci byl stanoven rok 1770, díky čemuž se podařilo zdokumentovat též češtinu z obrozeneckého období. V průběhu let byly sbírky lexikálního materiálu postupně doplňovány v souladu s aktuálními úkoly pracoviště. UNTRANSLATED'
+            }),
+            author: 'Ústav pro jazyk český AV ČR',
+            href: 'https://ujc.avcr.cz/elektronicke-slovniky-a-zdroje/kartoteka_lexikalniho_archivu.html'
+        })
+    }
+}
---
+++ src/js/tiles/custom/ujcKLA/model.ts
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2022 Martin Zimandl <martin.zimandl@gmail.com>
+ * Copyright 2022 Institute of the Czech National Corpus,
+ *                Faculty of Arts, Charles University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { IActionQueue, SEDispatcher, StatelessModel } from 'kombo';
+import { IAppServices } from '../../../appServices';
+import { Backlink, BacklinkWithArgs } from '../../../page/tile';
+import { RecognizedQueries } from '../../../query';
+import { DataStructure } from './common';
+import { Actions as GlobalActions } from '../../../models/actions';
+import { Actions } from './actions';
+import { List, HTTP } from 'cnc-tskit';
+import { isWebDelegateApi } from '../../../types';
+import { findCurrQueryMatch } from '../../../models/query';
+import { UjcKLAArgs, UjcKLAApi } from './api';
+
+
+export interface UjcKLAModelState {
+    isBusy:boolean;
+    ident:string;
+    data:DataStructure;
+    error:string;
+    backlinks:Array<BacklinkWithArgs<{}>>;
+}
+
+export interface UjcKLAModelArgs {
+    dispatcher:IActionQueue;
+    initState:UjcKLAModelState;
+    tileId:number;
+    api:UjcKLAApi,
+    appServices:IAppServices;
+    queryMatches:RecognizedQueries;
+    backlink:Backlink;
+}
+
+export class UjcKLAModel extends StatelessModel<UjcKLAModelState> {
+
+    private readonly tileId:number;
+
+    private readonly api:UjcKLAApi;
+
+    private readonly appServices:IAppServices;
+
+    private readonly backlink:Backlink;
+
+
+    constructor({dispatcher, initState, api, tileId, appServices, queryMatches, backlink}:UjcKLAModelArgs) {
+        super(dispatcher, initState);
+        this.tileId = tileId;
+        this.appServices = appServices;
+        this.api = api;
+        this.backlink = !backlink?.isAppUrl && isWebDelegateApi(this.api) ? this.api.getBackLink(backlink) : backlink;
+
+        this.addActionHandler(
+            GlobalActions.RequestQueryResponse,
+            (state, action) => {
+                const match = findCurrQueryMatch(List.head(queryMatches));
+                state.ident = match.lemma;
+                state.isBusy = true;
+                state.error = null;
+                state.data = {
+                    images: [],
+                }
+                state.backlinks = [];
+            },
+            (state, action, dispatch) => {
+                this.loadData(dispatch, state, state.ident);
+            }
+        );
+
+        this.addActionHandler(
+            Actions.TileDataLoaded,
+            (state, action) => {
+                if (action.payload.tileId === this.tileId) {
+                    state.isBusy = false;
+                    if (action.error) {
+                        state.error = action.error.message;
+
+                    } else {
+                        state.data = action.payload.data;
+                        state.backlinks = [this.generateBacklink(state.ident)];
+                    }
+                }
+            }
+        );
+
+        this.addActionHandler(
+            GlobalActions.GetSourceInfo,
+            (state, action) => {
+            },
+            (state, action, dispatch) => {
+                this.api.getSourceDescription(
+                    this.tileId,
+                    this.appServices.getISO639UILang(),
+                    ''
+                ).subscribe({
+                    next: (data) => {
+                        dispatch({
+                            name: GlobalActions.GetSourceInfoDone.name,
+                            payload: {
+                                data: data
+                            }
+                        });
+                    },
+                    error: (err) => {
+                        console.error(err);
+                        dispatch({
+                            name: GlobalActions.GetSourceInfoDone.name,
+                            error: err
+
+                        });
+                    }
+                });
+            }
+        );
+    }
+
+    private generateBacklink(ident:string):BacklinkWithArgs<{}> {
+        return {
+            url: `https://psjc.ujc.cas.cz/search.php`,
+            label: 'heslo v Příručním slovníku jazyka českého',
+            method: HTTP.Method.GET,
+            args: {
+                hledej: "Hledej",
+                heslo: ident,
+                where: "hesla",
+            }
+        };
+    }
+
+    private loadData(dispatch:SEDispatcher, state:UjcKLAModelState, q:string) {
+        const args:UjcKLAArgs = {
+            q
+        };
+        this.api.call(args).subscribe({
+            next: data => {
+                dispatch<typeof Actions.TileDataLoaded>({
+                    name: Actions.TileDataLoaded.name,
+                    payload: {
+                        tileId: this.tileId,
+                        isEmpty: false,
+                        data
+                    }
+                });
+            },
+            error: error => {
+                console.error(error);
+                dispatch<typeof Actions.TileDataLoaded>({
+                    name: Actions.TileDataLoaded.name,
+                    error,
+                    payload: {
+                        tileId: this.tileId,
+                        isEmpty: true,
+                        data: {
+                            images: [],
+                        },
+                    }
+                });
+            }
+        });
+    }
+
+}
