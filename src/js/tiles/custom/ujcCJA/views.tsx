fd54f06c9a9bbaac58994ffc87699d71de8a1915
Martin Zimandl <martin.zimandl@gmail.com>
2022-11-24 12:07:48 +0100

    added ujcCJA tile

---
+++ src/js/tiles/custom/ujcCJA/views.tsx
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 Martin Zimandl <martin.zimandl@gmail.com>
+ * Copyright 2022 Institute of the Czech National Corpus,
+ *                Faculty of Arts, Charles University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { IActionDispatcher, BoundWithProps, ViewUtils } from 'kombo';
+import * as React from 'react';
+import { Theme } from '../../../page/theme';
+import { CoreTileComponentProps, TileComponent } from '../../../page/tile';
+import { GlobalComponents } from '../../../views/common';
+import { UjcCJAModel, UjcCJAModelState } from './model';
+import * as S from './style';
+
+
+export function init(
+    dispatcher:IActionDispatcher,
+    ut:ViewUtils<GlobalComponents>,
+    theme:Theme, model:UjcCJAModel
+):TileComponent {
+
+    const globalComponents = ut.getComponents();
+
+    // -------------------- <UjcCJATileView /> -----------------------------------------------
+
+    const UjcCJATileView: React.FC<UjcCJAModelState & CoreTileComponentProps> = (props) => {
+
+        return (
+            <globalComponents.TileWrapper tileId={props.tileId} isBusy={props.isBusy} error={props.error}
+                hasData={!!props.data.content}
+                backlink={props.backlinks}
+                supportsTileReload={props.supportsReloadOnError}
+                issueReportingUrl={props.issueReportingUrl}
+                sourceIdent={{corp: 'UJC'}}>
+                <S.UjcCJATileView>
+                    <div className='container' dangerouslySetInnerHTML={{__html: props.data.content}}/>
+                    {props.data.image ? <img src={props.data.image} /> : null}
+                </S.UjcCJATileView>
+            </globalComponents.TileWrapper>
+        );
+    }
+
+    return BoundWithProps(UjcCJATileView, model);
+}
